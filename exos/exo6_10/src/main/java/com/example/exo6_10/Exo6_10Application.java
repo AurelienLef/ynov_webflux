7568eb7abf86f8400bfcb04112a372c1c62c6b65
Aurelien L <devbyaurel@gmail.com>
2026-03-05 15:55:15 +0100

    exo5 & 6

---
+++ exos/exo6_10/src/main/java/com/example/exo6_10/Exo6_10Application.java
@@ -0,0 +1,28 @@
+package com.example.exo6_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@SpringBootApplication
+@RestController
+public class Exo6_10Application {
+
+	@GetMapping("/api/processed-numbers")
+	public List<String> getProcessedNumbers() {
+		return IntStream.rangeClosed(1, 10)
+				.filter(n -> n % 2 == 0)
+				.map(n -> n * 10)
+				.mapToObj(n -> "Processed " + n)
+				.collect(Collectors.toList());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exo6_10Application.class, args);
+	}
+}
---
+++ exos/exo6_10/src/test/java/com/example/exo6/Exo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exo6_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class exo6_10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
